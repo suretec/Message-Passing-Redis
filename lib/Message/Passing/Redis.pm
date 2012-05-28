794bf47f817c728e2e087a8de2ce2f0ab2da0687
Tomas Doran <bobtfish@bobtfish.net>
2012-05-28 10:17:59 +0100

    Initial skeleton

---
+++ lib/Message/Passing/Redis.pm
@@ -0,0 +1,24 @@
+package Message::Passing::Redis;
+use strict;
+use warnings;
+use Redis;
+
+our $VERSION = '0.001';
+$VERSION = eval $VERSION;
+
+1;
+
+=head1 NAME
+
+Message::Passing::Redis - Adaptor for Redis PubSub
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Tomas (t0m) Doran <bobtfish@bobtfish.net>
+
+=cut
+
